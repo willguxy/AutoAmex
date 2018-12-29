b53a62e9fbedeee012efb967931e77f1049899c7
YF <yfu@vibrant-america.com>
2018-12-28 18:52:16 -0800

    update: support multi cards under one account

--- src/autoamex.py
+++ src/autoamex.py
@@ -1,47 +1,104 @@
 #!/usr/bin/env python
-import sys, time
+import sys
+import time
 from selenium import webdriver
 from datetime import datetime
 from helper import loadConfig, closeFeedback, clickOnOffers, amexLogIn, \
-  amexLogOut, getDriver, collectOfferNames
+    amexLogOut, getDriver, collectOfferNames, collectCards, changeCard
 
 amex_url = 'https://global.americanexpress.com/offers/eligible'
 
+
 def loginTest(cred, browser="PhantomJS"):
-  logfilename = datetime.strftime(datetime.now(), '%Y-%m-%d %H_%M_%S') + '.log'
-  with open('../tmp/' + logfilename, 'w+') as f:
-    sys.stdout = f 
-    driver = getDriver(browser)
-    i, t0  = 1, time.time()
-    for login_pair in cred:
-      t = time.time()
-      print("# {0} ID: {1}".format(i, login_pair[0]))
-      i += 1
-      try:
-        driver.get(amex_url)
-        time.sleep(2)
-        amexLogIn(driver, login_pair[0], login_pair[1], 'eliloUserID', 'eliloPassword')
-      except:
-        print("Something is wrong with login\n")
-        continue
-      time.sleep(5)
-      closeFeedback(driver)
-      time.sleep(3)
-      offer_names = collectOfferNames(driver)
-      print("Available offers are: {}".format(offer_names))
-      if offer_names: clickOnOffers(driver)
-      while not driver.find_elements_by_id('eliloUserID'):
-        try: amexLogOut(driver)
-        except: pass
-      print("Time used: %0.1f seconds\n" % (time.time() - t))
-    driver.quit()
-    print("** Summary **\nTotal time used: %0.1f seconds" % (time.time() - t0))
+    logfilename = datetime.strftime(
+        datetime.now(), '%Y-%m-%d %H_%M_%S') + '.log'
+    with open('../tmp/' + logfilename, 'w+') as f:
+        sys.stdout = f
+        driver = getDriver(browser)
+        i, t0 = 1, time.time()
+        for login_pair in cred:
+            t = time.time()
+            print("# {0} ID: {1}".format(i, login_pair[0]))
+            i += 1
+            try:
+                driver.get(amex_url)
+                time.sleep(2)
+                amexLogIn(driver, login_pair[0], login_pair[1],
+                          'eliloUserID', 'eliloPassword')
+            except:
+                print("Something is wrong with login\n")
+                continue
+            time.sleep(5)
+            closeFeedback(driver)
+            time.sleep(3)
+            offer_names = collectOfferNames(driver)
+            print("Available offers are: {}".format(offer_names))
+            if offer_names:
+                clickOnOffers(driver)
+            while not driver.find_elements_by_id('eliloUserID'):
+                try:
+                    amexLogOut(driver)
+                except:
+                    pass
+            print("Time used: %0.1f seconds\n" % (time.time() - t))
+        driver.quit()
+        print("** Summary **\nTotal time used: %0.1f seconds" %
+              (time.time() - t0))
+
+
+def loginTestMulti(cred, browser="PhantomJS"):
+    logfilename = datetime.strftime(
+        datetime.now(), '%Y-%m-%d %H_%M_%S') + '.log'
+    with open('../tmp/' + logfilename, 'w+') as f:
+        sys.stdout = f
+        driver = getDriver(browser)
+        i, t0 = 1, time.time()
+        for login_pair in cred:
+            t = time.time()
+            print("# {0} ID: {1}".format(i, login_pair[0]))
+            i += 1
+            try:
+                driver.get(amex_url)
+                time.sleep(2)
+                amexLogIn(
+                    driver, login_pair[0], login_pair[1], 'eliloUserID', 'eliloPassword')
+            except:
+                print("Something is wrong with login\n")
+                continue
+            time.sleep(5)
+            closeFeedback(driver)
+
+            time.sleep(3)
+            card_names = collectCards(driver)
+            # each card
+            for i in card_names:
+                tmpT = time.time()
+                print(i)
+                changeCard(driver, i)
+                time.sleep(5)
+                offer_names = collectOfferNames(driver)
+                print("Available offers are: {}".format(offer_names))
+                if offer_names:
+                    clickOnOffers(driver)
+                print("Time used: %0.1f seconds\n" % (time.time() - tmpT))
+            while not driver.find_elements_by_id('eliloUserID'):
+                try:
+                    amexLogOut(driver)
+                except:
+                    pass
+        # driver.quit()
+        print("** Summary **\nTotal time used: %0.1f seconds" %
+              (time.time() - t0))
+
 
 def main(argv):
-  browser = argv[0] if len(argv) >= 1 else 'Chrome'
-  loginTest(loadConfig("../conf/config.csv"), browser=browser)
+    browser = argv[0] if len(argv) >= 1 else 'Chrome'
+    action = argv[1] if len(argv) >= 2 else ''
+    if action == '':
+        loginTest(loadConfig("../conf/config.csv"), browser=browser)
+    else:
+        loginTestMulti(loadConfig("../conf/config.csv"), browser=browser)
+
 
 if __name__ == '__main__':
-  main(sys.argv[1:])
-
-
+    main(sys.argv[1:])
--- src/helper.py
+++ src/helper.py
@@ -1,127 +1,186 @@
-import csv, time, string
+import csv
+import time
+import string
 from random import choice
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 
 
 def GenPasswd2(length=8, chars=string.ascii_letters + string.digits):
-  return ''.join([choice(chars) for i in range(length)])
+    return ''.join([choice(chars) for i in range(length)])
 
 
 def genRandomText():
-  return GenPasswd2(8,string.digits) + GenPasswd2(15,string.ascii_letters)
+    return GenPasswd2(8, string.digits) + GenPasswd2(15, string.ascii_letters)
 
 
 def collectOfferNames(driver):
-  offer_elements = driver.find_elements_by_xpath("//*[contains(text(), 'Add to Card') \
+    offer_elements = driver.find_elements_by_xpath("//*[contains(text(), 'Add to Card') \
     or contains(text(), 'Save Promo Code')]/../../..")
-  tmpnames = [n.text.encode('ascii', 'ignore').decode('utf-8', 'ignore') for n in offer_elements if n.text]
-  tmpnames = [n.split('\n')[1] if '\n' in n else n for n in tmpnames]
-  offernames = ', '.join(sorted(tmpnames))
-  return offernames
+    tmpnames = [n.text.encode('ascii', 'ignore').decode(
+        'utf-8', 'ignore') for n in offer_elements if n.text]
+    tmpnames = [n.split('\n')[1] if '\n' in n else n for n in tmpnames]
+    offernames = ', '.join(sorted(tmpnames))
+    return offernames
+
+
+def collectCards(driver):
+
+    # open card slist
+    card_button_element = driver.find_element_by_xpath(
+        "//span[@class='card-stack-container']/span[@class='card-stack']/button")
+    card_button_element.click()
+    time.sleep(1)
+
+    if driver.find_element_by_xpath(
+            '//*[@title="View All"]'):
+        driver.find_element_by_xpath(
+            '//*[@title="View All"]').click()
+
+    # read card names
+    card_list = driver.find_element_by_id('accounts')
+
+    cards_name_elements = card_list.find_elements_by_class_name(
+        'heading-2')
+
+    card_names = []
+    for i in cards_name_elements:
+        card_names.append(i.text)
+
+    # close cards list
+    driver.execute_script("arguments[0].click();", card_button_element)
+
+    return card_names
+
+
+def changeCard(driver, cardName):
+    card_button_element = driver.find_element_by_xpath(
+        "//span[@class='card-stack-container']/span[@class='card-stack']/button")
+    card_button_element.click()
+    time.sleep(2)
+
+    card_list = driver.find_element_by_id('accounts')
+    card_list_element = card_list.find_element_by_xpath("//section/header/section/div/div/div/p[contains(text(),'" +
+                                                        cardName + "')]")
+    card_list_element.click()
+    print("clicked " + cardName)
 
 
 def getDriver(browser):
-  chrome_options = webdriver.ChromeOptions()
-  chrome_options.add_argument("--incognito")
-  chrome_options.add_argument("--window-size=1440,900")
-  if browser.lower() == 'firefox':
-    driver = webdriver.Firefox()
-  elif browser.lower() == 'chrome':
-    driver = webdriver.Chrome('./chromedriver', chrome_options=chrome_options)
-  elif browser.lower() == 'chrome_linux':
-    driver = webdriver.Chrome('./chromedriver_linux64', chrome_options=chrome_options)
-  elif browser.lower() in ('phantomjs', 'headless'):
-    driver = webdriver.PhantomJS()
-  else:
-    print("WARNING: browser selection not valid, use PhantomJS as default")
-    driver = webdriver.PhantomJS()
-  return driver
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.add_argument("--incognito")
+    chrome_options.add_argument("--window-size=1440,900")
+    if browser.lower() == 'firefox':
+        driver = webdriver.Firefox()
+    elif browser.lower() == 'chrome':
+        driver = webdriver.Chrome(
+            './chromedriver', chrome_options=chrome_options)
+    elif browser.lower() == 'chrome_linux':
+        driver = webdriver.Chrome(
+            './chromedriver_linux64', chrome_options=chrome_options)
+    elif browser.lower() in ('phantomjs', 'headless'):
+        driver = webdriver.PhantomJS()
+    else:
+        print("WARNING: browser selection not valid, use PhantomJS as default")
+        driver = webdriver.PhantomJS()
+    return driver
 
 
 def loadConfig(filename):
-  res = []
-  with open(filename, 'r') as f:
-    reader = csv.reader(f)
-    for row in reader: res.append(row)
-  return res
+    res = []
+    with open(filename, 'r') as f:
+        reader = csv.reader(f)
+        for row in reader:
+            res.append(row)
+    return res
 
 
 def closeFeedback(driver):
-  try:
-    driver.find_element_by_class_name("srCloseBtn").click()
-  except: pass
-  try:
-    driver.find_element_by_class_name("fsrCloseBtn").click()
-  except: pass
-  try:
-    driver.find_element_by_class_name("dls-icon-close").click()
-  except: pass
+    try:
+        driver.find_element_by_class_name("srCloseBtn").click()
+    except:
+        pass
+    try:
+        driver.find_element_by_class_name("fsrCloseBtn").click()
+    except:
+        pass
+    try:
+        driver.find_element_by_class_name("dls-icon-close").click()
+    except:
+        pass
 
 
 def clickOnOffers(driver):
-  for t in range(3):
-    if not collectOfferNames(driver): return
-    for e in driver.find_elements_by_xpath('//*[@title="Add to Card"]') + \
-            driver.find_elements_by_xpath('//*[@title="Save Promo Code"]'):
-      try: e.click()
-      except: pass
-      time.sleep(1)
-    if t != 2:
-      driver.refresh()
-      time.sleep(1)
+    for t in range(3):
+        if not collectOfferNames(driver):
+            return
+        for e in driver.find_elements_by_xpath('//*[@title="Add to Card"]') + \
+                driver.find_elements_by_xpath('//*[@title="Save Promo Code"]'):
+            try:
+                e.click()
+            except:
+                pass
+            time.sleep(1)
+        if t != 2:
+            driver.refresh()
+            time.sleep(1)
 
 
 def amexLogIn(driver, usr, pwd, emailFieldID='lilo_userName', passFieldID='lilo_password'):
-  WebDriverWait(driver, 10).until(lambda driver:
-    driver.find_element_by_id(emailFieldID)).clear()
-  WebDriverWait(driver, 10).until(lambda driver:
-    driver.find_element_by_id(emailFieldID)).send_keys(usr)
-  WebDriverWait(driver, 10).until(lambda driver:
-    driver.find_element_by_id(passFieldID)).clear()
-  WebDriverWait(driver, 10).until(lambda driver:
-    driver.find_element_by_id(passFieldID)).send_keys(pwd)
-  for e in driver.find_elements_by_xpath('//*[@type="submit"]'):
-    try: e.click()
-    except: pass
-  time.sleep(1)
-
-
-def amexLogOut(driver):
-  while driver.find_element_by_xpath('//*[contains(text(), "Log Out")]'):
-    try: driver.find_element_by_xpath('//*[contains(text(), "Log Out")]').click()
-    except: pass
+    WebDriverWait(driver, 10).until(lambda driver:
+                                    driver.find_element_by_id(emailFieldID)).clear()
+    WebDriverWait(driver, 10).until(lambda driver:
+                                    driver.find_element_by_id(emailFieldID)).send_keys(usr)
+    WebDriverWait(driver, 10).until(lambda driver:
+                                    driver.find_element_by_id(passFieldID)).clear()
+    WebDriverWait(driver, 10).until(lambda driver:
+                                    driver.find_element_by_id(passFieldID)).send_keys(pwd)
+    for e in driver.find_elements_by_xpath('//*[@type="submit"]'):
+        try:
+            e.click()
+        except:
+            pass
     time.sleep(1)
 
 
+def amexLogOut(driver):
+    while driver.find_element_by_xpath('//*[contains(text(), "Log Out")]'):
+        try:
+            driver.find_element_by_xpath(
+                '//*[contains(text(), "Log Out")]').click()
+        except:
+            pass
+        time.sleep(1)
+
+
 def twitterLogIn(driver, usr, pwd):
-  signInLinkID = "signin-link"
-  loginBtnClass = "submit"
-  emailField = "session[username_or_email]"
-  pwdField = "session[password]"
-  WebDriverWait(driver, 10).until(lambda driver:
-    driver.find_element_by_id(signInLinkID)).click()
-  WebDriverWait(driver, 10).until(lambda driver:
-    driver.find_element_by_name(emailField)).send_keys(usr)
-  WebDriverWait(driver, 10).until(lambda driver:
-    driver.find_element_by_name(pwdField)).send_keys(pwd)
-  WebDriverWait(driver, 10).until(lambda driver:
-    driver.find_element_by_class_name(loginBtnClass)).click()
+    signInLinkID = "signin-link"
+    loginBtnClass = "submit"
+    emailField = "session[username_or_email]"
+    pwdField = "session[password]"
+    WebDriverWait(driver, 10).until(lambda driver:
+                                    driver.find_element_by_id(signInLinkID)).click()
+    WebDriverWait(driver, 10).until(lambda driver:
+                                    driver.find_element_by_name(emailField)).send_keys(usr)
+    WebDriverWait(driver, 10).until(lambda driver:
+                                    driver.find_element_by_name(pwdField)).send_keys(pwd)
+    WebDriverWait(driver, 10).until(lambda driver:
+                                    driver.find_element_by_class_name(loginBtnClass)).click()
 
 
 def twitterLogOut(driver):
-  userDropdownID = "user-dropdown-toggle"
-  signOutBtn = "js-signout-button"
-  WebDriverWait(driver, 10).until(lambda driver:
-    driver.find_element_by_id(userDropdownID)).click()
-  WebDriverWait(driver, 10).until(lambda driver:
-    driver.find_element_by_class_name(signOutBtn)).click()
+    userDropdownID = "user-dropdown-toggle"
+    signOutBtn = "js-signout-button"
+    WebDriverWait(driver, 10).until(lambda driver:
+                                    driver.find_element_by_id(userDropdownID)).click()
+    WebDriverWait(driver, 10).until(lambda driver:
+                                    driver.find_element_by_class_name(signOutBtn)).click()
 
 
 def getBalance(driver):
-  try:
-    e = driver.find_element_by_xpath('//*[contains(text(), "Total Balance")]')
-    return e.find_element_by_xpath('../..').text.split('\n')[1]
-  except:
-    return "Error"
-
+    try:
+        e = driver.find_element_by_xpath(
+            '//*[contains(text(), "Total Balance")]')
+        return e.find_element_by_xpath('../..').text.split('\n')[1]
+    except:
+        return "Error"
