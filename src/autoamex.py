46d78ac2afc37bc1b26bcd09012448d812ecbf44
Brandon Arrindell <Brandon.Arrindell@silabs.com>
2023-11-29 14:57:52 -0600

    Refactor code to follow PEP8 and improve readability

--- src/autoamex.py
+++ src/autoamex.py
@@ -1,95 +1,70 @@
 #!/usr/bin/env python
-import sys, time
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-
+import sys
+import time
 from datetime import datetime
+import logging
 from helper import (
-    loadConfig,
-    closeFeedback,
-    clickOnOffers,
-    amexLogIn,
-    amexLogOut,
-    getDriver,
-    collectOfferNames,
+    load_config,
+    close_feedback,
+    click_on_offers,
+    amex_log_in,
+    amex_log_out,
+    get_driver,
+    collect_offer_names,
 )
 
-amex_url = "https://global.americanexpress.com/offers/eligible"
-
-# This function is defined to test the login functionality of a website.
-# The function takes two arguments - 'cred' and 'browser'.
-# 'cred' contains login credentials in pairs, one pair for each login.
-# 'browser' specifies which browser should be used; if it is not specified, 'PhantomJS' is used as default.
+AMEX_URL = "https://global.americanexpress.com/offers/eligible"
 
 
-def loginTest(cred, browser="PhantomJS"):
-    # A log file name is created, which includes current date and time to make it unique.
-    logfilename = datetime.strftime(datetime.now(), "%Y-%m-%d %H_%M_%S") + ".log"
+def login_test(credentials, browser="PhantomJS"):
+    log_file_name = datetime.strftime(datetime.now(), "%Y-%m-%d %H_%M_%S") + ".log"
+    logging.basicConfig(filename="../tmp/" + log_file_name, level=logging.INFO)
 
-    # The log file is opened in write plus mode, and system standard output is set to this file.
-    with open("../tmp/" + logfilename, "w+") as f:
-        sys.stdout = f
+    driver = get_driver(browser)
 
-        # The driver is initialized using the specified browser
-        driver = getDriver(browser)
+    t0 = time.time()  # Define t0 at the start of the function
 
-        i, t0 = 1, time.time()
+    for index, login_pair in enumerate(credentials, start=1):
+        logging.info("# {0} ID: {1}".format(index, login_pair[0]))
 
-        # Loop through each login_pair provided in cred
-        for login_pair in cred:
-            t = time.time()
+        t = time.time()  # Define t at the start of each loop iteration
 
-            # Print ID for each login
-            print("# {0} ID: {1}".format(i, login_pair[0]))
-            i += 1
+        try:
+            driver.get(AMEX_URL)
+            time.sleep(2)
+            amex_log_in(
+                driver, login_pair[0], login_pair[1], "eliloUserID", "eliloPassword"
+            )
 
-            try:
-                # Open the Amex URL, wait for 2 seconds, and then try to login with the provided credentials.
-                driver.get(amex_url)
-                time.sleep(2)
-                amexLogIn(
-                    driver, login_pair[0], login_pair[1], "eliloUserID", "eliloPassword"
-                )
+        except Exception as e:
+            logging.error(e)
+            logging.error("Something is wrong with login")
+            continue
 
-            except Exception as e:
-                # If there is an exception while logging in, print the error message and skip the current login_pair
-                print(e)
-                print("Something is wrong with login\n")
-                continue
+        time.sleep(5)
+        close_feedback(driver)
+        time.sleep(3)
+        offer_names = collect_offer_names(driver)
 
-            # Wait for 5 seconds, close the feedback pop-up, wait for 3 seconds and then retrieve offer names
-            time.sleep(5)
-            closeFeedback(driver)
-            time.sleep(3)
-            offer_names = collectOfferNames(driver)
+        logging.info("Available offers are: {}".format(offer_names))
 
-            # Print the available offers for this login_pair
-            print("Available offers are: {}".format(offer_names))
+        click_on_offers(driver)
 
-            # If there are any offers available, click on each offer.
-            # if offer_names:
-            clickOnOffers(driver)
+        try:
+            amex_log_out(driver)
+        except:
+            pass
 
-            try:
-                # Logout the user from Amex website.
-                amexLogOut(driver)
+        logging.info("Time used: %0.1f seconds" % (time.time() - t))
 
-            except:
-                pass
+    driver.quit()
 
-            # Compute and print the time taken to complete this login pair.
-            print("Time used: %0.1f seconds\n" % (time.time() - t))
-
-        # Close the driver
-        driver.quit()
-
-        # Compute and print the total time taken to test all the login pairs.
-        print("** Summary **\nTotal time used: %0.1f seconds" % (time.time() - t0))
+    logging.info("** Summary **\nTotal time used: %0.1f seconds" % (time.time() - t0))
 
 
 def main(argv):
     browser = argv[0] if len(argv) >= 1 else "Chrome"
-    loginTest(loadConfig("../conf/config.csv"), browser=browser)
+    login_test(load_config("../conf/config.csv"), browser=browser)
 
 
 if __name__ == "__main__":
